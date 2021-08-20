5e2ecd62cc9bd887fd5c2915a64d14829e90b445
omarmatter <omabd2001@gmail.com>
2021-08-21 00:13:26 +0300

    done

---
+++ challenges/dog-photo-gallery/script.js
@@ -0,0 +1,26 @@
+const btn= document.querySelector('#show');
+const list = document.querySelector('#list');
+
+const item = document.createElement('li')
+
+
+btn.addEventListener('click',function(){
+    ImgDog(); 
+})
+
+const url ='https://dog.ceo/api/breeds/image/random';
+function ImgDog() {
+  fetch(url)
+    .then(function (re) {
+      return re.json();
+    })
+    .then(function (data) {
+      
+      item.innerHTML =`<img src= ${ data.message} />` 
+      list.append(item)
+      }) .catch(error=>{
+        console.error("Error:", error)
+    })
+
+
+    }
